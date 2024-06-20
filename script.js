06cb3b0b102c1104c98b994510f66e00866de7ef
edercirino <edersantoscirino@gmail.com>
2024-06-19 23:19:23 -0300

    Adição de dois botões que iniciam as conversas com a mensagens prontas

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-function enviarMensagem(input){
-    if(input != ""){
-        window.open("http://wa.me/55"+input);
-    }else{
-        alert("Insira um número que esteja no Whatsapp");
-        return false;
-    }
+window.onload=function(){
+    let contactName = document.getElementById("contactName");
+    let whatsappNumber = document.getElementById("whatsappNumber");
+    let btnEnderecoIncompleto = document.getElementById("enderecoIncompleto");
+    let btnNaoTemNoMapa = document.getElementById("naoTemNoMapa");
+
+    btnEnderecoIncompleto.addEventListener('click', function(){
+        let contactNameValue = contactName.value;
+        let whatsappNumberValue = whatsappNumber.value;
+        let message = "Olá "+contactNameValue+". Aqui é entregadora Shopee. "+
+        "responsável pela sua entrega *HOJE*. Porém seu endereço está "+
+        "*INCOMPLETO*. Informe a *Quadra* e *Lote* se possível, "+
+        "envie a *localização* para facilitar, por favor?";
+
+        window.open("http://wa.me/55"+whatsappNumberValue+"?text="+message);
+    });
+
+    btnNaoTemNoMapa.addEventListener('click', function(){
+        let contactNameValue = contactName.value;
+        let whatsappNumberValue = whatsappNumber.value;
+        let message = "Olá "+contactNameValue+". Aqui é entregadora Shopee. "+
+        "não consegui localizar seu endereço no mapa. "+
+        "Pode me mandar a *Localização* por favor?";
+
+        window.open("http://wa.me/55"+whatsappNumberValue+"?text="+message);
+    });
 }
+
+ 
