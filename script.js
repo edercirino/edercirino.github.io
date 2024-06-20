941a404cc3ceb9d6efb9f3096e5b731d0e7a252d
edercirino <edersantoscirino@gmail.com>
2024-06-20 07:49:44 -0300

    Inclusão do botão 'abrir conversa'

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ window.onload=function(){
     let whatsappNumber = document.getElementById("whatsappNumber");
     let btnEnderecoIncompleto = document.getElementById("enderecoIncompleto");
     let btnNaoTemNoMapa = document.getElementById("naoTemNoMapa");
+    let btnAbrirConversa = document.getElementById("abrirConversa");
 
     btnEnderecoIncompleto.addEventListener('click', function(){
         let contactNameValue = contactName.value;
         let whatsappNumberValue = whatsappNumber.value;
-        let message = "Olá "+contactNameValue+". Aqui é entregadora Shopee. "+
+        let message = "Olá "+contactNameValue+". Aqui é entregadora Shopee "+
         "responsável pela sua entrega *HOJE*. Porém seu endereço está "+
-        "*INCOMPLETO*. Informe a *Quadra* e *Lote* se possível, "+
+        "*INCOMPLETO*.\n Informe a *Quadra* e *Lote* se possível, "+
         "envie a *localização* para facilitar, por favor?";
 
         window.open("http://wa.me/55"+whatsappNumberValue+"?text="+message);
@@ -18,11 +19,17 @@ window.onload=function(){
     btnNaoTemNoMapa.addEventListener('click', function(){
         let contactNameValue = contactName.value;
         let whatsappNumberValue = whatsappNumber.value;
-        let message = "Olá "+contactNameValue+". Aqui é entregadora Shopee. "+
+        let message = "Olá "+contactNameValue+". Aqui é entregadora Shopee, "+
         "não consegui localizar seu endereço no mapa. "+
         "Pode me mandar a *Localização* por favor?";
 
         window.open("http://wa.me/55"+whatsappNumberValue+"?text="+message);
+    });
+
+    btnAbrirConversa.addEventListener('click', function(){
+        let whatsappNumberValue = whatsappNumber.value;
+
+        window.open("http://wa.me/55"+whatsappNumberValue);
     });
 }
 
